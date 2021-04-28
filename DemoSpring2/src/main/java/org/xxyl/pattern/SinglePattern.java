0a33f5e46a6a1cf9a68846ce6642b4c31f87786d
xxylChen <1772944281@qq.com>
2021-04-28 17:39:39 +0800

    提交resources

---
+++ DemoSpring2/src/main/java/org/xxyl/pattern/SinglePattern.java
@@ -0,0 +1,10 @@
+package org.xxyl.pattern;
+
+public class SinglePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DemoSpring2/src/main/java/org/xxyl/sort/bubbleSort.java
@@ -0,0 +1,10 @@
+package org.xxyl.sort;
+
+public class bubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
